84528ac5761be2ebe14804638cba08dbaaf9bc4d
gkn2 <112503427+gkn2@users.noreply.github.com>
2025-03-20 11:50:19 -0400

    Update main.ts, main.py, images.g.jres, images.g.ts

--- main.py
+++ main.py
@@ -4,23 +4,31 @@ SCREEN_WIDTH = 160
 SCREEN_HEIGHT = 120
 
 
+def get_duck_sprite():
+    if assets.image("duck") is not None:
+        return assets.image("duck")  
+    else:
+        # Placeholder sprite in case "duck" is missing
+        return assets.image("""duck""")
+
+
 x = 0
 while x < SCREEN_WIDTH:
-    sprite = sprites.create(assets.image("""duck"""), SpriteKind.player)
+    sprite = sprites.create(get_duck_sprite(), SpriteKind.player)
     sprite.set_position(x, 20)
     x += SPRITE_WIDTH
 
 
 y = 0
 while y < SCREEN_HEIGHT:
-    sprite = sprites.create(assets.image("""duck"""), SpriteKind.player)
+    sprite = sprites.create(get_duck_sprite(), SpriteKind.player)
     sprite.set_position(20, y)
     y += SPRITE_HEIGHT
 
 
 i = 0
 while i < min(SCREEN_WIDTH, SCREEN_HEIGHT):
-    sprite = sprites.create(assets.image("""duck"""), SpriteKind.player)
+    sprite = sprites.create(get_duck_sprite(), SpriteKind.player)
     sprite.set_position(i, i)
     i += SPRITE_WIDTH
 
@@ -29,7 +37,7 @@ x = 0
 while x < SCREEN_WIDTH:
     y = 0
     while y < SCREEN_HEIGHT:
-        sprite = sprites.create(assets.image("""duck"""), SpriteKind.player)
+        sprite = sprites.create(get_duck_sprite(), SpriteKind.player)
         sprite.set_position(x, y)
         y += SPRITE_HEIGHT * 2
     x += SPRITE_WIDTH * 2
@@ -39,8 +47,8 @@ x = 0
 while x < SCREEN_WIDTH:
     y = 0
     while y < SCREEN_HEIGHT:
-        if (x + y) % (SPRITE_WIDTH * 4) == 0:
-            sprite = sprites.create(assets.image("""duck"""), SpriteKind.player)
+        if (x + y) % (SPRITE_WIDTH * 4) == 0:  
+            sprite = sprites.create(get_duck_sprite(), SpriteKind.player)
             sprite.set_position(x, y)
         y += SPRITE_HEIGHT * 2
     x += SPRITE_WIDTH * 2
--- main.ts
+++ main.ts
@@ -3,21 +3,31 @@ let SPRITE_WIDTH = 16
 let SPRITE_HEIGHT = 16
 let SCREEN_WIDTH = 160
 let SCREEN_HEIGHT = 120
+function get_duck_sprite(): Image {
+    if (assets.image`duck` !== null) {
+        return assets.image`duck`
+    } else {
+        //  Placeholder sprite in case "duck" is missing
+        return assets.image`duck`
+    }
+    
+}
+
 let x = 0
 while (x < SCREEN_WIDTH) {
-    sprite = sprites.create(assets.image`duck`, SpriteKind.Player)
+    sprite = sprites.create(get_duck_sprite(), SpriteKind.Player)
     sprite.setPosition(x, 20)
     x += SPRITE_WIDTH
 }
 let y = 0
 while (y < SCREEN_HEIGHT) {
-    sprite = sprites.create(assets.image`duck`, SpriteKind.Player)
+    sprite = sprites.create(get_duck_sprite(), SpriteKind.Player)
     sprite.setPosition(20, y)
     y += SPRITE_HEIGHT
 }
 let i = 0
 while (i < Math.min(SCREEN_WIDTH, SCREEN_HEIGHT)) {
-    sprite = sprites.create(assets.image`duck`, SpriteKind.Player)
+    sprite = sprites.create(get_duck_sprite(), SpriteKind.Player)
     sprite.setPosition(i, i)
     i += SPRITE_WIDTH
 }
@@ -25,7 +35,7 @@ x = 0
 while (x < SCREEN_WIDTH) {
     y = 0
     while (y < SCREEN_HEIGHT) {
-        sprite = sprites.create(assets.image`duck`, SpriteKind.Player)
+        sprite = sprites.create(get_duck_sprite(), SpriteKind.Player)
         sprite.setPosition(x, y)
         y += SPRITE_HEIGHT * 2
     }
@@ -36,7 +46,7 @@ while (x < SCREEN_WIDTH) {
     y = 0
     while (y < SCREEN_HEIGHT) {
         if ((x + y) % (SPRITE_WIDTH * 4) == 0) {
-            sprite = sprites.create(assets.image`duck`, SpriteKind.Player)
+            sprite = sprites.create(get_duck_sprite(), SpriteKind.Player)
             sprite.setPosition(x, y)
         }
         
